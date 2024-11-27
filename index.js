ee2fcbc7ecd5f251c3740b95b6565c2519880f91
amadousadialloh <amadousadialloh@gmail.com>
2024-11-26 18:11:29 -0600

    Exercise 1 completed

---
+++ index.js
@@ -0,0 +1,10 @@
+//Exercise 1
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+const newNumsArray = nums.map((num) => {
+  const result = num * 2;
+  console.log(result);
+});
